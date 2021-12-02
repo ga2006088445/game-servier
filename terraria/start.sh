6b640e9c14d6c687c2e42c37fc30201c86c45eb9
TungCheng <ga2006088445@gmail.com>
2021-12-02 23:57:32 +0800

    (^) 修正主程式

--- terraria/start.sh
+++ terraria/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/root/ServerLinux/TerrariaServer -config /root/.config/TerrariaServer/serverconfig.txt
+# /root/ServerLinux/TerrariaServer -config /root/.config/TerrariaServer/serverconfig.txt
+/root/Linux/TerrariaServer -config /root/.config/TerrariaServer/serverconfig.txt
