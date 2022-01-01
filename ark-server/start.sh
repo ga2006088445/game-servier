1f0f04364b534fef988773aedcb66a676836d850
TungCheng <ga2006088445@gmail.com>
2022-01-01 18:59:57 +0800

    (^) test

--- ark-server/start.sh
+++ ark-server/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-byobu new-session -s "/root/ark_server/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen -culture=zh-TW"
+/root/ark_server/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen -culture=zh-TW
 
--- ark-server/updata&start.sh
+++ ark-server/updata&start.sh
@@ -2,4 +2,4 @@
 
 /root/steamcmd/steamcmd.sh +force_install_dir "/root/ark_server" +login anonymous +app_update 376030 validate +quit
 
-byobu new-session -s "/root/ark_server/ShooterGameServer TheIsland?listen -culture=zh-TW"
+/root/ark_server/ShooterGameServer TheIsland?listen -culture=zh-TW
