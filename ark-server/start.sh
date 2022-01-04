755c98504e920eb1dadd454e990f0175665e3d1b
TungCheng <ga2006088445@gmail.com>
2022-01-04 19:38:54 +0800

    (^) 改仙境

--- ark-server/start.sh
+++ ark-server/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/root/ark_server/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen -culture=zh-TW
+/root/ark_server/ShooterGame/Binaries/Linux/ShooterGameServer Ragnarok?listen -culture=zh-TW
 
