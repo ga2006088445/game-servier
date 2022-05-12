6061a42d2a4df5b366b36930a62719ea5e03c781
TungCheng <ga2006088445@gmail.com>
2022-05-12 20:00:30 +0800

    cmd

--- project-zomboid/start.sh
+++ project-zomboid/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-
-byobu new-session -s "project-zomboid" "/root/project-zomboid/start-server.sh -servername tomorrow_server"
+while true
+do
+    echo '==='
+    sleep 1000000
+done
+# byobu new-session -s "project-zomboid" "/root/project-zomboid/start-server.sh -servername tomorrow_server"
